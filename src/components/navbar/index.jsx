4a76289ecbe1a80f9d475ba2b1d5ce2c66a9495f
Mohd Sahil <mohdsahil@Mohds-MacBook.local>
2020-05-27 14:52:37 +0530

    mobile menu completed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import sprite from "../logo-and-icons/skill-logo-and-icons.svg";
 
-import './index.scss';
+import "./index.scss";
 
 export default ({ inView }) => {
   let [navLinks, setNavLinks] = useState([
-    { link: '#home', text: 'home', status: 'inactive' },
-    { link: '#skills', text: 'skills', status: 'inactive' },
-    { link: '#work', text: 'work', status: 'inactive' },
-    { link: '#about', text: 'about', status: 'inactive' },
-    { link: '#contact', text: 'contact', status: 'inactive' },
+    { link: "#home", text: "home", status: "inactive" },
+    { link: "#skills", text: "skills", status: "inactive" },
+    { link: "#work", text: "work", status: "inactive" },
+    { link: "#about", text: "about", status: "inactive" },
+    { link: "#contact", text: "contact", status: "inactive" },
   ]);
 
-  const addClass = inView ? 'nav-bar' : 'nav-bar nav-bar-inverse';
+  const addClass = inView ? "nav-bar" : "nav-bar nav-bar-inverse";
 
-  const makeActive = clickedLink => {
+  const makeActive = (clickedLink) => {
     const updatedLinks = [...navLinks];
-    updatedLinks.forEach(link =>
+    updatedLinks.forEach((link) =>
       clickedLink === link.text
-        ? (link.status = 'active')
-        : (link.status = 'inactive')
+        ? (link.status = "active")
+        : (link.status = "inactive")
     );
     setNavLinks(updatedLinks);
   };
 
   return (
-    <nav className={addClass}>
-      <div className='left-links'>
+    <div>
+      <div className='burger-menu'>
+        <svg>
+          <use href={`${sprite}#burger-menu`} />
+        </svg>
         <ul>
-          {navLinks.map(nav => (
+          {navLinks.map((nav) => (
             <li className={nav.status} key={nav.text}>
               <a
                 href={nav.link}
-                onClick={event => makeActive(event.target.text)}
+                onClick={(event) => makeActive(event.target.text)}
               >
                 {nav.text}
               </a>
@@ -39,6 +43,22 @@ export default ({ inView }) => {
           ))}
         </ul>
       </div>
-    </nav>
+      <nav className={addClass}>
+        <div>
+          <ul className='left-links'>
+            {navLinks.map((nav) => (
+              <li className={nav.status} key={nav.text}>
+                <a
+                  href={nav.link}
+                  onClick={(event) => makeActive(event.target.text)}
+                >
+                  {nav.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
