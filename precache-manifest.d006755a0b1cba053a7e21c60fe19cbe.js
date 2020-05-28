52dd17dd3f5c4e972bac1a0763c86d9eeb039e0f
Mohd Sahil <sahil242927@gmail.com>
2020-05-29 00:39:53 +0530

    Updates

---
+++ precache-manifest.d006755a0b1cba053a7e21c60fe19cbe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606107773d0fae115126fd3096ae48d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3a94b850b2edeb4d1b4",
+    "url": "/portfolio/static/css/3.40219913.chunk.css"
+  },
+  {
+    "revision": "7fa5c7ebf54fe23c1339",
+    "url": "/portfolio/static/css/main.54120532.chunk.css"
+  },
+  {
+    "revision": "e89804a4eabde1c1a36c",
+    "url": "/portfolio/static/js/2.3932c89c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3932c89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a94b850b2edeb4d1b4",
+    "url": "/portfolio/static/js/3.70d8d3a3.chunk.js"
+  },
+  {
+    "revision": "7fa5c7ebf54fe23c1339",
+    "url": "/portfolio/static/js/main.0ea9dff7.chunk.js"
+  },
+  {
+    "revision": "ffe5afedadf077c929a2",
+    "url": "/portfolio/static/js/runtime-main.ddb5eaba.js"
+  },
+  {
+    "revision": "a1c1b63db9394ec9be7a5158a295f5f4",
+    "url": "/portfolio/static/media/skill-logo-and-icons.a1c1b63d.svg"
+  }
+]);
